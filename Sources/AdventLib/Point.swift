9529e226fbe6f8086c2cba984bdf87b1dd0a1382
David Paschich <dpassage@balveda.com>
2018-12-09 22:07:56 -0800

    add rect and point

---
+++ Sources/AdventLib/Point.swift
@@ -0,0 +1,15 @@
+//
+//  Point.swift
+//  AdventLib
+//
+//  Created by David Paschich on 12/9/18.
+//
+
+import Foundation
+
+public struct Point {
+    public var x: Int
+    public var y: Int
+}
+
+extension Point: Hashable {}
