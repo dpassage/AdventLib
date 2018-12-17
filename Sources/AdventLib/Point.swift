c1b4038cf3cbc20d0fa22731ae2a856dc3836eea
David Paschich <dpassage@balveda.com>
2018-12-16 17:44:34 -0800

    rect slice test, failing. needs a whole new type

--- Sources/AdventLib/Point.swift
+++ Sources/AdventLib/Point.swift
@@ -10,6 +10,11 @@ import Foundation
 public struct Point {
     public var x: Int
     public var y: Int
+
+    public init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
 }
 
 extension Point: Hashable {}
--- Tests/AdventLibTests/RectTests.swift
+++ Tests/AdventLibTests/RectTests.swift
@@ -11,16 +11,15 @@ import AdventLib
 class RectTests: XCTestCase {
 
     func testSlice() {
+        let rect = Rect(width: 10, height: 10, defaultValue: true)
 
-        
+        let subRect = rect[Point(x: 1, y: 1)...Point(x: 2, y: 5)]
 
+        let start = subRect.startIndex
+        XCTAssertEqual(start, Point(x: 1, y: 1))
+        let next = subRect.index(after: start)
+        XCTAssertEqual(next, Point(x: 2, y: 1))
+        let nextNext = subRect.index(after: next)
+        XCTAssertEqual(nextNext, Point(x: 1, y: 2))
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
